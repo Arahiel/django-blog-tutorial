60cd840d60f6cd9133faf2e23b4a21e44e7a6582
Sebastian Tobolski <sebastian.tobolski@globallogic.com>
2021-08-23 10:51:00 +0200

    Added base structure and linking for the urls

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,18 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
+
+
+def index(request):
+    return render(request, "blog/index.html")
+
+
+def posts(request):
+    return render(request, "blog/post.html", {
+        "page_title": "All posts"
+    })
+
+def post(request, slug):
+    return render(request, "blog/post.html", {
+        "page_title": "Post title"
+    })
