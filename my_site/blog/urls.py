bf238d687d12c794eed2e56d695a3ec7b55f92ee
Sebastian Tobolski <sebastian.tobolski@globallogic.com>
2021-09-07 10:10:21 +0200

    Implemented read later feature

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post, name="post-detail-page"),
+    path("posts/add-read_later", views.AddReadLaterView.as_view(), name="add-read-later"),
+    path("posts/remove-read-later", views.RemoveReadLaterView.as_view(), name="remove-read-later"),
+    path("posts/<slug:slug>", views.PostView.as_view(), name="post-detail-page"),
     path("tag/<str:tag_name>", views.tag, name="tag-page")
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,9 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views.generic.base import View
 from .models import Post
+from django.views.generic import DetailView
 # Create your views here.
 
 
@@ -17,14 +21,34 @@ def posts(request):
     })
 
 
-def post(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": post})
+class AddReadLaterView(View):
+    def post(self, request):
+        slug = request.POST["read_later_slug"]
+        request.session[f"read_later_slugs.{slug}"] = True
+        return HttpResponseRedirect(reverse("post-detail-page", args=(slug,)))
+
+class RemoveReadLaterView(View):
+    def post(self, request):
+        slug = request.POST["read_later_slug"]
+        request.session[f"read_later_slugs.{slug}"] = False
+        return HttpResponseRedirect(reverse("post-detail-page", args=(slug,)))
+
+
+class PostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        session = self.request.session
+        post = self.object
+        context["to_be_read"] = bool(session.get(f"read_later_slugs.{post.slug}"))
+        return context
 
 
 def tag(request, tag_name):
-    posts_data = Post.objects.all().order_by("-date").filter(tags__caption=tag_name)
+    posts_data = Post.objects.all().order_by(
+        "-date").filter(tags__caption=tag_name)
     return render(request, "blog/posts.html", {
         "posts": posts_data
     })
